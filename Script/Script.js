a5d0b3d0c3bfe5c019ca1189d8bfccc978a736b2
Sijey <cjpraveen@hotmail.com>
2021-12-08 18:19:48 +0530

    Update Script.js

--- Script/Script.js
+++ Script/Script.js
@@ -18,16 +18,44 @@ const operations = [":", "(", ")", "{", "}"]
 const ignore = ["\n", "\r", "\t"]
 const d_types = ["func ", "Class ", "str ", "int ", "float ", "dict ", "array "]
 
+// Characters
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 // ErrorHandler
 function ErrorHandler(type, argv=[]) {
-    if (type === "value-err") {
+    if (type === "NoValueErr") {
         console.log(`Error : No Value Provided for Variable '${argv[0]}', Variable Type : ${argv[1]}`)
         exit()
     }
 }
 
+// Read Variable Value
+function ReadVariableValue(dta, name, return_) {
+    JSON.parse(dta,
+        function(key, value) {
+            if (key === name[0]) {
+                return_(value)
+                // for (let i = 0;i < value;i++) {
+                //     JSON.parse(value[i],
+                //         function(key, value) {
+                //             if (key === name[1]) {
+                //                 return_(value)
+                //             }
+                //         }
+                //     )
+                // }
+            }
+
+            else {
+                return_(`No Variable Found!, With name '${name[0]}'`)
+            }
+        }
+    )
+}
+
 // Tokenizer
-function Tokenizer(code) {
+function __main__(code) {
     // let operator = []
     let string_vars = []
     let integer_vars = []
@@ -44,53 +72,61 @@ function Tokenizer(code) {
         for (let j = 0;j < d_types.length;j++) {
             if (__render__[i].includes(d_types[j])) {
                 
-                if (d_types[j] == "str ") {
+                if (__render__[i].substring(0, 4) === "str ") {
                     _var = __render__[i].replace(d_types[j], "").split(" = ")
-                    if (_var.length < 2) { ErrorHandler("value-err", [_var[0], d_types[j]]) }
-                    else if (_var[1] === "") { ErrorHandler("value-err", [_var[0], d_types[j]]) }
+                    if (_var.length < 2) { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
+                    else if (_var[1] === "") { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
                     else { string_vars.push( { "var_name" : _var[0], "var_value" : _var[1] } ) }    
                 }
                 
-                else if (d_types[j] == "int ") {
+                else if (__render__[i].substring(0, 4) === "int ") {
                     _var = __render__[i].replace(d_types[j], "").split(" = ")
-                    if (_var.length < 2) { ErrorHandler("value-err", [_var[0], d_types[j]]) }
-                    else if (_var[1] === "") { ErrorHandler("value-err", [_var[0], d_types[j]]) }
+                    if (_var.length < 2) { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
+                    else if (_var[1] === "") { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
                     else { integer_vars.push( { "var_name" : _var[0], "var_value" : _var[1] } ) }    
                 }
                 
-                else if (d_types[j] == "array ") {
+                else if (__render__[i].substring(0, 6) === "array ") {
                     _var = __render__[i].replace(d_types[j], "").split(" = ")
-                    if (_var.length < 2) { ErrorHandler("value-err", [_var[0], d_types[j]]) }
-                    else if (_var[1] === "") { ErrorHandler("value-err", [_var[0], d_types[j]]) }
+                    if (_var.length < 2) { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
+                    else if (_var[1] === "") { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
                     else { array_vars.push( { "var_name" : _var[0], "var_value" : _var[1] } ) }    
                 }
                 
-                else if (d_types[j] == "dict ") {
+                else if (__render__[i].substring(0, 5) === "dict ") {
                     _var = __render__[i].replace(d_types[j], "").split(" = ")
-                    if (_var.length < 2) { ErrorHandler("value-err", [_var[0], d_types[j]]) }
-                    else if (_var[1] === "") { ErrorHandler("value-err", [_var[0], d_types[j]]) }
+                    if (_var.length < 2) { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
+                    else if (_var[1] === "") { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
                     else { dict_vars.push( { "var_name" : _var[0], "var_value" : _var[1] } ) }    
                 }
                 
-                else if (d_types[j] == "float ") {
+                else if (__render__[i].substring(0, 6) === "float ") {
                     _var = __render__[i].replace(d_types[j], "").split(" = ")
-                    if (_var.length < 2) { ErrorHandler("value-err", [_var[0], d_types[j]]) }
-                    else if (_var[1] === "") { ErrorHandler("value-err", [_var[0], d_types[j]]) }
+                    if (_var.length < 2) { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
+                    else if (_var[1] === "") { ErrorHandler("NoValueErr", [_var[0], d_types[j]]) }
                     else { float_vars.push( { "var_name" : _var[0], "var_value" : _var[1] } ) }    
                 }
 
             }
         }
     }
-
-    // Executes the 'print()' statement
+    
+    // 'print()' statement
     for (let i = 0;i < __render__.length;i++) {
-        if (__render__[i].includes("print(")) {
+        if (__render__[i].substring(0, 6) === "print(") {
             var _value = __render__[i].replace("print", "").substring(1 , __render__[i].length - 6)
-            if (_value.split("")[0] == '"') {
-                console.log(_value.substring(1, _value.length - 1))
+            if (_value.substring(0, 4) === "var(") {
+                _value = _value.replace("var", "").substring(1 , _value.length - 4)
+                console.log(_value)
+                ReadVariableValue(JSON.stringify({"string_vars" : string_vars, "integer_vars" : integer_vars, "array_vars" : array_vars, "float_vars" : float_vars, "dict_vars" : dict_vars}), ["string_vars", _value], (data) => {
+                    console.log(data)
+                })
             } else {
-                console.log(`'${_value.substring(0, _value.length)}' is a Variable.`)
+                if (_value.includes("//")) {
+                    console.log(_value.replace("//", "\n"))
+                } else {
+                    console.log(_value)
+                }
             }
         }
     }
@@ -98,17 +134,30 @@ function Tokenizer(code) {
     return {"string_vars" : string_vars, "integer_vars" : integer_vars, "array_vars" : array_vars, "float_vars" : float_vars, "dict_vars" : dict_vars}
 }
 
+
+
 // Arguments
 var argv = process.argv
 
-if (argv[2].toLowerCase() === "-e") {
+if (argv.length < 3) {
+    console.log("No Arguments Provided!, Use --help or -h to get Help.")
+}
+
+else if (argv[2].toLowerCase() === "-e") {
     if (argv.length > 3) {
         readFile(argv[3], {encoding : "utf-8"}, (err, fc) => {
             if (err) { console.log(err) }
             else {
-                var tokens = Tokenizer(fc.toString().split("\r\n"))
-                console.log(tokens)    
+                console.log(__main__(fc.toString().split("\r\n")))
             }
         })
     }
+}
+
+else if (argv[2].toLowerCase() === "-v" || argv[2].toLowerCase() === "--version") {
+    console.log("0.1a0")
+}
+
+else {
+    console.log(`Invalid Argument '${argv[2]}', Use --help or -h to get Help.`)
 }
